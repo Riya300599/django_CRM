b1b9fe1ec5e32907b3bb8dbaa564eef9b4796692
manojchavan139 <chavan.manoj139@gmail.com>
2021-11-15 21:25:12 +0530

    first commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.LoginView, name='login'),
-    path('register/', views.RegisterView, name='register'),
-    path('logout/', views.LogoutView, name='logout'),
+    path('login/', LoginView, name='login'),
+    path('register/', RegisterView, name='register'),
+    path('logout/', LogoutView, name='logout'),
 
     # url for role == admin
-    path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
-    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('', home, name='home'),
+    path('products/', products, name='products'),
+    path('customer/<str:pk>/', customer, name='customer'),
 
-    path('create_order/<str:pk>/', views.create_order, name='create_order'),
-    path('update_order/<str:pk>/', views.update_order, name='update_order'),
-    path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
+    path('create_order/<str:pk>/', create_order, name='create_order'),
+    path('update_order/<str:pk>/', update_order, name='update_order'),
+    path('delete_order/<str:pk>/', delete_order, name='delete_order'),
 
-    path('create_product/', views.create_product, name='create_product'),
-    path('update_product/<str:pk>/', views.update_product, name='update_product'),
-    path('delete_product/<str:pk>/', views.delete_product, name='delete_product'),
+    path('create_product/', create_product, name='create_product'),
+    path('update_product/<str:pk>/', update_product, name='update_product'),
+    path('delete_product/<str:pk>/', delete_product, name='delete_product'),
 
 
     # url for role == customer
-    path('user/', views.user_page, name='user'),
-    path('account/', views.account_settings, name='account'),
+    path('user/', user_page, name='user'),
+    path('account/', account_settings, name='account'),
 ]
